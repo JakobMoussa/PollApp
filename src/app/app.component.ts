0ab975d5721fe9c1b35c58b9bb226679225fd9df
jakob <jakobmoussa5@gmail.com>
2026-08-30 17:10:23 +0200

    chore: remove unused app.component.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+/**
+ * The root component of the application.
+ */
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'poll-app';
